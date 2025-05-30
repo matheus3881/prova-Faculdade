a9ce2cd96564ab69b33fd50ae768541cb81760fc
Sant <matheus3881.ms@gmail.com>
2025-05-30 09:54:16 -0300

    feat: Armazena dados do input  no firebase
    Foi feita a conexão com o Firevase e já é possível armazenar dados do input la. Próximo é fazer validações e melhorar o style da página

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,20 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar,IonInput   } from '@ionic/angular/standalone';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonInput,
+  IonButton
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonInput  ]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonInput,
+    IonButton
+  ],
 })
 export class LoginPage implements OnInit {
+  constructor(private firestore: Firestore) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  nome: string = '';
+  email: string = '';
+
+  async salvarUsuario(nome: string, email: string) {
+    const usuarioRef = collection(this.firestore, 'usuario');
+
+    try {
+      await addDoc(usuarioRef, {
+        nome: nome,
+        email: email,
+        criadoEm: new Date(),
+      });
+      console.log('usuário criado com sucesso');
+    } catch (error) {
+      console.error('Erro ao salvar o usuário', error);
+    }
   }
-
 }
